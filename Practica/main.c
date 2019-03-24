8e31b6988e18ea9289892ed943884c9aa7c3366a
TrovatoCarla <cntrovato@buenosaires.gob.ar>
2019-03-24 18:24:06 -0300

    Funciones ClaseSabado

--- Practica/main.c
+++ Practica/main.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int PedirNumeros();
+float SacarPromedio(int suma, int cantidad);
+
+
 int main()
 {
-    printf("HOLA,BUEN DOMINGO");
+
+    promedio=SacarPromedio(94,5);
+    SacarPromedio();
     return 0;
+
 }
+
+int PedirNumeros()
+{
+    int numeroIngresado;
+    int acumulador=0;
+
+    for(int i=0;i<5;i++)
+    {
+        do
+        {
+        printf("\Ingrese un numero del 0 al 10: ");
+        scanf("%d",&numeroIngresado);
+        }
+        while(numeroIngresado>10 || numeroIngresado<0);
+        acumulador=acumulador+numeroIngresado;
+    }
+}
+float SacarPromedio(int suma, int cantidad)
+{
+    float promedio;
+    promedio=(float)suma/cantidad;
+    printf("El promedio es:%f",promedio);
+    return promedio;
+
+}
+
